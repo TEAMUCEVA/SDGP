fdceb8a5c51d1f123f8dd172b6989ce7205ec065
Alvaro Salazar <43754402+alvaro-salazar@users.noreply.github.com>
2022-05-04 09:53:55 -0500

    Este es el primer commit del proyecto

---
+++ src/test/java/co/edu/uceva/sdgp/SdgpApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.sdgp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdgpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
